f0a5dc04fb4df320519246d5a5e08deb4b192ffc
AnchorRen <543177205@qq.com>
2016-07-17 22:35:06 +0800

    POM file  And initial classes

---
+++ src/main/java/com/anchorren/QAFlatformApplication.java
@@ -0,0 +1,12 @@
+package com.anchorren;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QAFlatformApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QAFlatformApplication.class, args);
+	}
+}
